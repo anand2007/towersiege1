fba42d993dd3a6fdc0346f56ef13e67785dc9853
anand2007 <72135786+anand2007@users.noreply.github.com>
2020-10-24 22:33:08 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,24 @@
+class Box{
+    constructor(x, y, width, height) {
+        var options = {
+            //'restitution':0.2,
+            'friction':0.5,
+            'density':0.1
+        }
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        World.add(world, this.body);
+      }
+      display(){
+        var angle = this.body.angle;
+        push();
+        translate(this.body.position.x, this.body.position.y);
+        rotate(angle);
+        rectMode(CENTER);
+        
+        fill("green");
+        rect(0,0,this.width,this.height)
+        pop();
+      }
+}
